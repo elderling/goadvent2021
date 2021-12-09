71d84ade872caac03d369adeb067e752eaf09169
Jonathan W. Taylor <elderling@gmail.com>
2021-12-09 09:00:52 -0600

    Added string reverse function

--- day03.go
+++ day03.go
@@ -3,3 +3,16 @@ package main
 func Day03aSolution(filename string) int {
 	return 198
 }
+
+func ReverseString(s string) string {
+	if s == "" {
+		return ""
+	}
+	reversedBytes := make([]byte, 0)
+	r := []byte(s)
+	for i := len(r) - 1; i >= 0; i-- {
+		reversedBytes = append(reversedBytes, r[i])
+	}
+	theReturn := string(reversedBytes)
+	return theReturn
+}
--- day03_test.go
+++ day03_test.go
@@ -10,3 +10,31 @@ func TestDay03aSolution(t *testing.T) {
 		t.Error("bad TestDay03aSolution")
 	}
 }
+
+func TestReverseString(t *testing.T) {
+
+	r := ReverseString("")
+	if r != "" {
+		t.Errorf("Failed to reverse empty string. Got '%v'", r)
+	}
+
+	r = ReverseString("a")
+	if r != "a" {
+		t.Errorf("Failed to reverse 'a' Got: '%v'", r)
+	}
+
+	r = ReverseString("ab")
+	if r != "ba" {
+		t.Errorf("Failed to reverse 'ab' Got: '%v'", r)
+	}
+
+	r = ReverseString("racecar")
+	if r != "racecar" {
+		t.Errorf("Failed to reverse 'racecar' Got: '%v'", r)
+	}
+
+	r = ReverseString("abcdef")
+	if r != "fedcba" {
+		t.Errorf("Failed to reverse 'abcdef' Got: '%v'", r)
+	}
+}
