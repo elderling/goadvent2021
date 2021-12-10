239953db72ea6efa202d3a94a93a71c46fdcaf7c
Jonathan Taylor <jonathant@datafoundry.com>
2021-12-10 11:03:24 -0600

    Simplify reverse code

--- day03.go
+++ day03.go
@@ -5,14 +5,10 @@ func Day03aSolution(filename string) int {
 }
 
 func ReverseString(s string) string {
-	if s == "" {
-		return ""
-	}
 	reversedBytes := make([]byte, 0)
 	r := []byte(s)
 	for i := len(r) - 1; i >= 0; i-- {
 		reversedBytes = append(reversedBytes, r[i])
 	}
-	theReturn := string(reversedBytes)
-	return theReturn
+	return string(reversedBytes)
 }
