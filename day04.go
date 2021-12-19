37f94b67e1d64c68b9029374bd6c8cf96af8d006
Jonathan W. Taylor <elderling@gmail.com>
2021-12-19 13:56:55 -0600

    Add vertical bingo test

---
+++ day04.go
@@ -0,0 +1,30 @@
+package main
+
+const BINGO_CARD_ROWS = 5
+const BINGO_CARD_COLS = 5
+
+type BingoCard struct {
+	//numbers [BINGO_CARD_ROWS][BINGO_CARD_COLS]int
+	marked [5][5]bool
+}
+
+func (card *BingoCard) HasVerticalBingo() bool {
+
+	maxRow := 0
+
+	for i := 0; i < BINGO_CARD_ROWS; i++ {
+		maxRow = 0
+		for j := 0; j < BINGO_CARD_COLS; j++ {
+			if card.marked[j][i] {
+				maxRow++
+			} else {
+				break
+			}
+		}
+		if maxRow == BINGO_CARD_ROWS {
+			break
+		}
+	}
+
+	return maxRow == BINGO_CARD_ROWS
+}
---
+++ day04_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestHasHorizontalBingo(t *testing.T) {
+	card := &BingoCard{}
+
+	card.marked[0][0] = true
+	card.marked[1][0] = true
+	card.marked[2][0] = true
+	card.marked[3][0] = true
+	card.marked[4][0] = true
+
+	if !card.HasVerticalBingo() {
+		t.Error("First vertical Bingo not detected")
+	}
+
+	card.marked[0][0] = false
+
+	if card.HasVerticalBingo() {
+		t.Error("False positive")
+	}
+
+	card.marked[0][2] = true
+	card.marked[1][2] = true
+	card.marked[2][2] = true
+	card.marked[3][2] = true
+	card.marked[4][2] = true
+
+	if !card.HasVerticalBingo() {
+		t.Error("Second vertical Bingo not detected")
+	}
+
+	card.marked[0][2] = false
+
+	if card.HasVerticalBingo() {
+		t.Error("False positive")
+	}
+
+	card = &BingoCard{}
+	card.marked[0][0] = true
+	card.marked[0][1] = true
+	card.marked[0][2] = true
+	card.marked[0][3] = true
+	card.marked[0][4] = true
+
+	if card.HasVerticalBingo() {
+		t.Error("False positive")
+	}
+
+}
